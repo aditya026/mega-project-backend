c80a7e29f6b8a623d11ed9629bc14712d9f80a3a
aditya_singh <adityasingh260504@gmail.com>
2026-01-06 09:56:17 +0530

    resetPassword controller on 06-01-2026

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel")
 const mailSender = require("../utils/mailSender")
+const bcrypt = require("bcrypt")
 
 // reset password token
 exports.resetPasswordToken = async (req,res) => {
@@ -48,5 +49,54 @@ exports.resetPasswordToken = async (req,res) => {
     }
 }
 // reset password
+exports.resetPassword = async (req,res) => {
+    try{
+        // data fetch
+    const {password, confirmPassword, token} = req.body
+    
+    // validation
+    if(password !== confirmPassword){
+        return res.json({
+            success: false,
+            message: "Password is not matching",
+        })
+    }
+    // get user details from db using token
+    const userDetails = await user.findOne({token: token})
+    // if no entry - invalid token
+    if(!userDetails){
+         return res.json({
+            success: false,
+            message: "token is invalid"
+         })
+    }
+    // token time check 
+    if(userDetails.resetPasswordExpires < Date.now()){
+        return res.json({
+            success: false,
+            message: "token is expired, please regenerate your token"
+        });
+    }
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10)
+    // password update
+    await User.findOneAndUpdate(
+        {token:token},
+        {password:hashedPassword},
+        {new:true},
+    )
+    // response return
+    return res.status(200).json({
+        success: true,
+        message: "password reset successfull"
+    })
+
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "reset password is not done"
+        })
+    }
+}
 
 // last 5 min revision
